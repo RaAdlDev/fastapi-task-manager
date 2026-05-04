cc5a274fa2258bd74957ee57142ad8874cbc0fbc
RaAdDev <adlgarciago@gmail.com>
2026-05-03 19:47:18 -0600

    Add conection

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,7 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 
 class Settings(BaseSettings):
     secret_key: str
+    database_url: str
     debug: bool = False
 
     model_config = SettingsConfigDict(env_file=".env")
